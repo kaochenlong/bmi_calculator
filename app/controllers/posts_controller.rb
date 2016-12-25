a9fdbea9b01fa1bdf2910f06412b90a85ee662ea
Eddie Kao <eddie@digik.com.tw>
2016-12-26 00:16:39 +0800

    add kaminari gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.page(params[:page]).per(5)
   end
 
   # GET /posts/1
